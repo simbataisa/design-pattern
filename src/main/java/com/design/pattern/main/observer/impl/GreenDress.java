063d42993a50b208b7a90e3457f328dfb40d3d1f
Dennis Dao <simbataisa@gmail.com>
2018-06-21 16:01:41 +0800

    Overvable

--- src/main/java/com/design/pattern/main/observer/impl/GreenDress.java
+++ src/main/java/com/design/pattern/main/observer/impl/GreenDress.java
@@ -1,16 +1,11 @@
 package com.design.pattern.main.observer.impl;
 
-import com.design.pattern.main.observer.Observable;
-import com.design.pattern.main.observer.Observer;
-
-import java.util.ArrayList;
-import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
 
 /**
  * Created by bekuku on 5/6/15.
  */
-public class GreenDress extends Item implements Observable {
+public class GreenDress extends Item {
 
     public GreenDress() {
         this.users = new CopyOnWriteArrayList<>();
--- src/main/java/com/design/pattern/main/observer/impl/RedDress.java
+++ src/main/java/com/design/pattern/main/observer/impl/RedDress.java
@@ -1,13 +1,11 @@
 package com.design.pattern.main.observer.impl;
 
-import com.design.pattern.main.observer.Observable;
-
 import java.util.concurrent.CopyOnWriteArrayList;
 
 /**
  * Created by bekuku on 5/6/15.
  */
-public class RedDress extends Item implements Observable {
+public class RedDress extends Item {
 
 
     public RedDress() {
